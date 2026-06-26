86053b99a17d1c5a2075e24be735ae9edb1115af
pgrtech09 <pgr.tech09@gmail.com>
2026-06-26 06:34:36 +0530

    Add files via upload

---
+++ src/lib/FIX_01_supabase.ts
@@ -0,0 +1,7 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "@/types";
+
+export const createClient = () =>
+  createClientComponentClient<Database>();
+
+export const createServerClient = () => createClientComponentClient<Database>();
